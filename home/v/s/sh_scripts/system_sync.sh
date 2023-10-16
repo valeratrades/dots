3d0d9b351fc4691171f40fd33c9e78dd83b89df3
Valera6 <v79166789533@gmail.com>
2023-10-16 02:05:25 +0000

    2023-10-16 02:05:25

--- home/v/s/sh_scripts/system_sync.sh
+++ home/v/s/sh_scripts/system_sync.sh
@@ -4,4 +4,5 @@ ${HOME}/.dots/sync_git.sh && printf "\033[32msynced dots\033[0m\n" || printf "\0
 
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak && sudo sh -c "rankmirrors -n 10 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist" && printf "\033[32mupdated mirrors\033[0m\n" || printf "\033[31mupdating mirrors failed\033[0m\n"
 
-sudo pacman -Rns --noconfirm ttf-jetbrains-mono-nerd > /dev/null 2>&1 && sudo pacman -Syu --noconfirm && printf "\033[32mupdated system\033[0m\n" || printf "\033[31mpacman -Syu failed\033[0m\n"
+sudo pacman -Rns --noconfirm ttf-jetbrains-mono-nerd > /dev/null 2>&1
+sudo pacman -Syu --noconfirm && printf "\033[32mupdated system\033[0m\n" || printf "\033[31mpacman -Syu failed\033[0m\n"
