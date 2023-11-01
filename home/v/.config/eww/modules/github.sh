e35ab7d94d1a037b717229227f2255b8aadc9e67
Valera6 <v79166789533@gmail.com>
2023-11-01 00:19:38 +0000

    2023-11-01 00:19:38

--- home/v/.config/eww/modules/github.sh
+++ home/v/.config/eww/modules/github.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 notifications="$(gh api notifications 2> /dev/null | jq '. | length')"
-if [ -n $notifications ]then
-	echo "$notifications" 
-fi
+[ -z notifications ] && echo "" || echo "$notifications" 
