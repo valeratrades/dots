b16c3e75691a5681dfd040a678013491e96561cb
Valera6 <v79166789533@gmail.com>
2023-11-03 19:40:27 +0000

    .

--- main.sh
+++ main.sh
@@ -44,7 +44,8 @@ dot_directories="
 "
 
 commit() {
-	local message=$(date +"%Y-%m-%d %H:%M:%S")
+	# local message=$(date +"%Y-%m-%d %H:%M:%S")
+	local message="." # At this moment it makes more sense to me to auto-commit with '.', so other comments are easily searchable
 	if [ -n "$1" ]; then
 		message="$@"
 	fi
