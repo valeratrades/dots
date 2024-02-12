5437186e3d71a18c6762e86704062e6d1aa78132
Valera6 <v79166789533@gmail.com>
2024-02-12 01:46:55 +0000

    .

--- home/v/.config/nvim/lua/valera/lazy.lua
+++ home/v/.config/nvim/lua/valera/lazy.lua
@@ -160,6 +160,25 @@ return require('lazy').setup({
 	'jbyuki/instant.nvim',
 	"andweeb/presence.nvim",
 	{
+		'cameron-wags/rainbow_csv.nvim',
+		config = true,
+		ft = {
+			'csv',
+			'tsv',
+			'csv_semicolon',
+			'csv_whitespace',
+			'csv_pipe',
+			'rfc_csv',
+			'rfc_semicolon'
+		},
+		cmd = {
+			'RainbowDelim',
+			'RainbowDelimSimple',
+			'RainbowDelimQuoted',
+			'RainbowMultiDelim'
+		}
+	},
+	{
 		"kylechui/nvim-surround",
 		version = "*",
 		event = "VeryLazy",
--- home/v/.file_snippets/run.sh
+++ home/v/.file_snippets/run.sh
@@ -2,4 +2,5 @@
 # File is source on `cs` into the project's root. Allows to define a set of project-specific commands and aliases.
 
 #alias r="sc lrun -- start"
-alias a="c add"
+alias a="cargo add"
+alias patch="cargo set-version --bump patch && gg && cargo publish"
