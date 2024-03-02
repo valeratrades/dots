c66e7cd7edab7d223d9ee4c7135a30cada42c6de
Valera6 <v79166789533@gmail.com>
2024-03-02 10:56:44 +0000

    actually working improved init projects

---
+++ home/v/.file_snippets/go_main
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	world := "World"
+	fmt.Printf("Hello %s\n", world)
+}
--- home/v/s/help_scripts/init_projects.sh
+++ home/v/s/help_scripts/init_projects.sh
@@ -5,6 +5,7 @@ can() {
 	project_name="${1}"
 	cd "$project_name" || printf "\033[31m'can' assumes project_name being the first argument\033[0m\n"
 	cp ${HOME}/.file_snippets/rust_local.sh ./.local.sh
+	source ./.local.sh
 	ln ${HOME}/.file_snippets/rustfmt.toml ./rustfmt.toml
 	ln ${HOME}/.file_snippets/rust_gitignore ./.gitignore -f
 	git add -A
@@ -15,9 +16,10 @@ pyn() {
 	mkdir "$1"
 	cd "$1"
 	cp ${HOME}/.file_snippets/py_local.sh ./.local.sh
+	source ./.local.sh
 	cp ${HOME}/.file_snippets/pyproject.toml ./pyproject.toml
 	ln ${HOME}/.file_snippets/python_gitignore ./.gitignore
-	cp ${HOME}/.file_snippets/main_py ./main.py
+	cp ${HOME}/.file_snippets/go_main ./main.py &&  chmod u+x ./main.py
 	git init
 	git add -A
 	git commit -m "-- New Project Snippet --"
@@ -27,8 +29,10 @@ gon() {
 	mkdir "$1"
 	cd "$1"
 	cp ${HOME}/.file_snippets/go_local.sh ./.local.sh
+	source ./.local.sh
 	ln ${HOME}/.file_snippets/gofumpt.toml ./gofumpt.toml
 	ln ${HOME}/.file_snippets/go_gitignore ./.gitignore
+	mkdir cmd && cp ${HOME}/.file_snippets/go_main ./cmd/main.go && chmod u+x ./cmd/main.go
 	git init
 	git add -A
 	git commit -m "-- New Project Snippet --"
