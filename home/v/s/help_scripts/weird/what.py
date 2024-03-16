807d3581677e39feead5015f3abe0447478b0781
Valera6 <v79166789533@gmail.com>
2024-03-16 09:10:17 +0000

    cli: better what

--- home/v/s/help_scripts/weird/what.py
+++ home/v/s/help_scripts/weird/what.py
@@ -1,15 +1,18 @@
 import time
 import sys
 
+RATE = 0.995
+
 
 def type_out_text(text, delay=0.1):
+    new_delay = delay
     for char in text:
         sys.stdout.write(char)
         sys.stdout.flush()
-        time.sleep(delay)
+        time.sleep(new_delay)
         if char == "\n":
-            time.sleep(delay * 10)
-        delay = delay * 0.995
+            time.sleep(new_delay * 10)
+        new_delay -= new_delay * (1 - RATE) * (new_delay / delay)
 
 
 if __name__ == "__main__":
