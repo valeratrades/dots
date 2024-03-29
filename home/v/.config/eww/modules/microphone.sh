f239d750a67bb7b36adfd254ba6934925cac612b
Valera6 <v79166789533@gmail.com>
2024-03-29 23:13:59 +0000

    .

--- home/v/.config/eww/modules/microphone.sh
+++ home/v/.config/eww/modules/microphone.sh
@@ -6,13 +6,12 @@ if [ "$mute" = "true" ]; then
 	volume=""
 	icon=""
 else 
-	#volume=$(pamixer --default-source --get-volume)
+	volume=$(pamixer --default-source --get-volume)
 	icon=""
 fi
 
-if [ -n "$volume" ] && [ "$volume" -gt 40 ]; then
-	volume="30"
-	#pamixer --default-source --set-volume 30
-fi
+#if [ -n "$volume" ] && [ "$volume" -gt 40 ]; then
+#	volume="30"
+#fi
 
 echo "{\"content\": \"$volume\", \"icon\": \"$icon\"}"
