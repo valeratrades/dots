2eb8c9d31f0e2299a2f32795843f9243da5090bc
Valera6 <v79166789533@gmail.com>
2024-05-04 10:08:40 +0000

    cli:init_project refactor

--- home/v/.file_snippets/init_projects.sh
+++ home/v/.file_snippets/init_projects.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#TODO: add presets. For example, --clap on `can` would add clap with "derive" to dependencies, then the main func would have hell world example specifically with clap, so I don't have to remember all the intricasies
 
 shared_before() {
 	# $1: project_name
@@ -7,7 +8,7 @@ shared_before() {
 	project_name="${1}"
 	cd "$project_name" || printf "\033[31mproject_name has to be passed as the first argument\033[0m\n"
 
-	lang="${2}" # rust, py, go
+	lang="${2}" # rs, py, go
 
 	cp ${HOME}/.file_snippets/local_sh/${lang}.sh ./.local.sh
 	source ./.local.sh
@@ -17,7 +18,8 @@ shared_before() {
 	echo "\n" >> ./.gitignore
 	cat ${HOME}/.file_snippets/gitignore/${lang} >> ./.gitignore
 
-	cat ${HOME}/.file_snippets/readme/header_start.md > README.md
+	cat ${HOME}/.file_snippets/readme/header.md > README.md
+	cat ${HOME}/.file_snippets/readme/badges/${lang} | reasonable_envsubst - >> README.md
 }
 
 shared_after() {
@@ -28,7 +30,6 @@ shared_after() {
 	lang="${2}" # rust, py, go
 	touch TODO.md
 
-	cat ${HOME}/.file_snippets/readme/header_end.md >> README.md
 	sed -i "s/PROJECT_NAME_PLACEHOLDER/${project_name}/g" README.md
 	cat ${HOME}/.file_snippets/readme/licenses.md >> README.md
 	sudo ln ${HOME}/.file_snippets/readme/LICENSE-APACHE ./LICENSE-APACHE
@@ -41,37 +42,41 @@ shared_after() {
 
 can() {
 	cargo new "${@}"
-	shared_before ${1} "rust"
+	lang="rs"
+	shared_before ${1} ${lang}
 
-	sudo ln ${HOME}/.file_snippets/rust/rustfmt.toml ./rustfmt.toml
-	sudo ln ${HOME}/.file_snippets/rust/deny.toml ./deny.toml
-	cat ${HOME}/.file_snippets/rust/default_dependencies >> Cargo.toml
-	cat ${HOME}/.file_snippets/readme/rust_readme >> README.md
+	sudo ln ${HOME}/.file_snippets/${lang}/rustfmt.toml ./rustfmt.toml
+	sudo ln ${HOME}/.file_snippets/${lang}/deny.toml ./deny.toml
+	cat ${HOME}/.file_snippets/${lang}/default_dependencies >> Cargo.toml
+	cp -f ${HOME}/.file_snippets/${lang}/main ./src/main.${lang} && chmod u+x ./src/main.${lang}
 
-	shared_after ${1} "rust"
+	shared_after ${1} ${lang}
 }
 
 pyn() {
 	mkdir "$1"
-	shared_before ${1} "py"
+	lang = "py"
+	shared_before ${1} ${lang}
 
-	sudo ln ${HOME}/.file_snippets/pyproject.toml ./pyproject.toml
+	sudo ln ${HOME}/.file_snippets/${lang}/pyproject.toml ./pyproject.toml
 	mkdir ./src
-	cp ${HOME}/.file_snippets/py_main ./src/main.py &&  chmod u+x ./src/main.py
+	cp ${HOME}/.file_snippets/${lang}/main ./src/main.${lang} &&  chmod u+x ./src/main.${lang}
 
 	git init
-	shared_after ${1} "py"
+	shared_after ${1} ${lang}
 }
 
 gon() {
 	mkdir "$1"
-	shared_before ${1} "go"
+	lang="go"
+	shared_before ${1} ${lang}
 
-	sudo ln ${HOME}/.file_snippets/gofumpt.toml ./gofumpt.toml
-	mkdir cmd && cp ${HOME}/.file_snippets/go_main ./cmd/main.go && chmod u+x ./cmd/main.go
+	sudo ln ${HOME}/.file_snippets/${lang}/gofumpt.toml ./gofumpt.toml
+	# go's convention of cmd/ as the access point prevents placeing this operation in shared_before
+	mkdir cmd && cp ${HOME}/.file_snippets/${lang}/main ./cmd/main.${lang} && chmod u+x ./cmd/main.${lang}
 
 	git init
-	shared_after ${1} "go"
+	shared_after ${1} ${lang}
 	go mod init "github.com/${GITHUB_NAME}/$1"
 }
 
--- home/v/.file_snippets/readme/licenses.md
+++ home/v/.file_snippets/readme/licenses.md
@@ -1,5 +1,17 @@
-## License
-This project is available under the [MIT](LICENSE-MIT) or [Apache 2.0](LICENSE-APACHE) licenses, at your choice.
 
-## Contributing
-Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
+<br>
+
+#### License
+
+<sup>
+Licensed under either of <a href="LICENSE-APACHE">Apache License, Version
+2.0</a> or <a href="LICENSE-MIT">MIT license</a> at your option.
+</sup>
+
+<br>
+
+<sub>
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in this crate by you, as defined in the Apache-2.0 license, shall
+be dual licensed as above, without any additional terms or conditions.
+</sub>
