9bb1991b37b2a26c31455d43960838ef1284e4a1
valeratrades <v79166789533@gmail.com>
2024-07-03 18:49:42 +0000

    _

--- home/v/s/help_scripts/maintenance/check_caches.sh
+++ home/v/s/help_scripts/maintenance/check_caches.sh
@@ -12,7 +12,7 @@ fi
 
 # (in kilobytes, so 20GB)
 home_cache_threshold=20000000
-home_cache_size=$(du -sk /home/v/.cache | cut -f1)
+home_cache_size=$(sudo du -sk /home/v/.cache | cut -f1)
 if [ "$home_cache_size" -gt "$home_cache_threshold" ]; then
 	rm -rf /home/v/.cache/*
 	echo "\033[32mCleaned home cache\033[0m"
